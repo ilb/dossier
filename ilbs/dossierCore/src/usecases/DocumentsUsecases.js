0caeafebc0b0d985d3790a5b5001f73f160439ae
Stas Kozlov <barakok202@gmail.com>
2023-12-12 14:46:50 +0300

    Обновил логи

--- ilbs/dossierCore/src/usecases/DocumentsUsecases.js
+++ ilbs/dossierCore/src/usecases/DocumentsUsecases.js
@@ -10,9 +10,9 @@ export default class DocumentsUsecases extends Usecases {
    * @returns {}
    */
   async list({ documentsService, request }) {
-    debug('get list start');
+    debug('get list start', request.uuid);
     const list = await documentsService.getDocuments({ ...request, withVersions: true });
-    debug('get list end');
+    debug('get list end', request.uuid);
     return list;
   }
 
@@ -35,9 +35,9 @@ export default class DocumentsUsecases extends Usecases {
    * @param {object} request
    */
   async update({ pagesService, request }) {
-    debug('update document start');
+    debug('update document start', request.uuid);
     await pagesService.add(request);
-    debug('update document end');
+    debug('update document end', request.uuid);
   }
 
   /**
@@ -59,9 +59,9 @@ export default class DocumentsUsecases extends Usecases {
    * @param {object} request
    */
   async correctPages({ pagesService, request }) {
-    debug('correctPages start');
+    debug('correctPages start', request.uuid);
     await pagesService.correct(request);
-    debug('correctPages end');
+    debug('correctPages end', request.uuid);
   }
 
   /**
@@ -69,9 +69,9 @@ export default class DocumentsUsecases extends Usecases {
    * @param {object} request
    */
   async deletePage({ pagesService, request }) {
-    debug('deletePage start');
+    debug('deletePage start', request.uuid);
     await pagesService.delete(request);
-    debug('deletePage end');
+    debug('deletePage end', request.uuid);
   }
 
   /**
@@ -79,9 +79,9 @@ export default class DocumentsUsecases extends Usecases {
    * @param {object} request
    */
   async getPage({ pagesService, request }) {
-    debug('getPage start');
+    debug('getPage start', request.uuid);
     const page = await pagesService.get(request);
-    debug('getPage end');
+    debug('getPage end', request.uuid);
 
     return page;
   }
@@ -91,45 +91,45 @@ export default class DocumentsUsecases extends Usecases {
    * @returns {Promise<{file: Buffer, filename: *, contentType: *}>}
    */
   async print({ documentsService, request }) {
-    debug('getDocument (print) start');
+    debug('getDocument (print) start', request.uuid);
     const document = await documentsService.getDocument(request);
-    debug('getDocument (print) end');
+    debug('getDocument (print) end', request.uuid);
 
     return document;
   }
 
   async getByVersion({ documentsService, request }) {
-    debug('getDocumentByVersion start');
+    debug('getDocumentByVersion start', request.uuid);
     const documentByVersion = await documentsService.getDocumentByVersion(request);
-    debug('getDocumentByVersion end');
+    debug('getDocumentByVersion end', request.uuid);
     return documentByVersion;
   }
 
   async getDate({ dossierService, request }) {
-    debug('getCreatedDate start');
+    debug('getCreatedDate start', request.uuid);
     const createdDate = await dossierService.getCreatedDate(request);
-    debug('getCreatedDate end');
+    debug('getCreatedDate end', request.uuid);
     return createdDate;
   }
 
   async changeVersion({ documentsService, request }) {
-    debug('changeDocumentVersion start');
+    debug('changeDocumentVersion start', request.uuid);
     const newVersion = await documentsService.changeDocumentVersion(request);
-    debug('changeDocumentVersion end');
+    debug('changeDocumentVersion end', request.uuid);
     return newVersion;
   }
 
   async documentsInfo({ documentsService, request }) {
-    debug('getDocumentsInfo start');
+    debug('getDocumentsInfo start', request.uuid);
     const documentsInfo = await documentsService.getDocumentsInfo(request);
-    debug('getDocumentsInfo end');
+    debug('getDocumentsInfo end', request.uuid);
     return documentsInfo;
   }
 
   async changeState({ documentsService, request }) {
-    debug('changeDocumentState start');
+    debug('changeDocumentState start', request.uuid);
     const result = await documentsService.changeDocumentState(request);
-    debug('changeDocumentState end');
+    debug('changeDocumentState end', request.uuid);
     return result;
   }
 }
