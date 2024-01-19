43637b1a480989b27e4589f7b3a6cd75cb27e162
Bonamente <56506035@protonmail.com>
2024-01-19 23:02:48 +0500

    refactor(ControlsMenu): удалил лишний код

--- ilbs/dossierUi/src/classifier/client/components/gallery/GalleryItem/ControlsMenu.js
+++ ilbs/dossierUi/src/classifier/client/components/gallery/GalleryItem/ControlsMenu.js
@@ -14,70 +14,63 @@ function ControlsMenu({
 }) {
   return (
     <>
-      <div className='file-dossier-file-controls controlsMenu'>
-        <div className='menuItem menuItemHorizontal minusItem'>
+      <div className="file-dossier-file-controls controlsMenu">
+        <div className="menuItem menuItemHorizontal minusItem">
           <button
-            className='navBtn'
+            className="navBtn"
             onClick={(e) => {
               zoomImageOut(e, { scaleNum: scale });
-            }}
-          >
-            <i className='icon iconMinus' />
+            }}>
+            <i className="icon iconMinus" />
           </button>
         </div>
 
-        <div className='menuItem menuItemHorizontal plusItem'>
+        <div className="menuItem menuItemHorizontal plusItem">
           <button
-            className='navBtn'
+            className="navBtn"
             onClick={(e) => {
               zoomImageIn(e, { scaleNum: scale });
-            }}
-          >
-            <i className='icon iconPlus' />
+            }}>
+            <i className="icon iconPlus" />
           </button>
         </div>
 
-        <div className='menuItemHorizontal zoomDropdownWrapper'>
-          <div className='zoomCounter'>{(scale * 100).toFixed()}%</div>
+        <div className="menuItemHorizontal zoomDropdownWrapper">
+          <div className="zoomCounter">{(scale * 100).toFixed()}%</div>
           <select
-            className='zoomDropdown'
+            className="zoomDropdown"
             onChange={(e) => {
               zoomImageWithDropdown(e.target.value);
-            }}
-          >
-            <option value='0.5'>50%</option>
-            <option value='0.75'>75%</option>
-            <option value='1' selected>
-              100%
-            </option>
-            <option value='1.25'>125%</option>
-            <option value='1.5'>150%</option>
-            <option value='2'>200%</option>
-            <option value='3'>300%</option>
-            <option value='4'>400%</option>
+            }}>
+            <option value="0.5">50%</option>
+            <option value="0.75">75%</option>
+            <option value="1">100%</option>
+            <option value="1.25">125%</option>
+            <option value="1.5">150%</option>
+            <option value="2">200%</option>
+            <option value="3">300%</option>
+            <option value="4">400%</option>
           </select>
         </div>
 
         {pageCount > 1 && (
-          <div className='menuItem menuItemHorizontal pageNavigation'>
+          <div className="menuItem menuItemHorizontal pageNavigation">
             <button
-              className='navBtn'
+              className="navBtn"
               onClick={() => navigatePage('prev')}
-              disabled={currentPage === 1}
-            >
-              <i className='icon iconLeft' />
+              disabled={currentPage === 1}>
+              <i className="icon iconLeft" />
             </button>
 
-            <div className='page-counter'>
+            <div className="page-counter">
               <b>{currentPage}</b>
             </div>
 
             <button
-              className='navBtn'
+              className="navBtn"
               onClick={() => navigatePage('next')}
-              disabled={currentPage === pageCount}
-            >
-              <i className='icon iconRight' />
+              disabled={currentPage === pageCount}>
+              <i className="icon iconRight" />
             </button>
           </div>
         )}
@@ -86,33 +79,30 @@ function ControlsMenu({
           style={{
             display: 'flex',
             marginLeft: 'auto',
-          }}
-        >
-          <div className='menuItem menuItemHorizontal undoItem'>
+          }}>
+          <div className="menuItem menuItemHorizontal undoItem">
             <button
-              className='navBtn'
+              className="navBtn"
               onClick={(e) => {
                 rotateImage(e, { angle: -90 });
-              }}
-            >
-              <i className='icon iconUndo' />
+              }}>
+              <i className="icon iconUndo" />
             </button>
           </div>
 
-          <div className='menuItem menuItemHorizontal redoItem'>
+          <div className="menuItem menuItemHorizontal redoItem">
             <button
-              className='navBtn'
+              className="navBtn"
               onClick={(e) => {
                 rotateImage(e, { angle: 90 });
-              }}
-            >
-              <i className='icon iconRedo' />
+              }}>
+              <i className="icon iconRedo" />
             </button>
           </div>
 
-          <div className='menuItem menuItemHorizontal'>
-            <button className='navBtn' onClick={closePreview}>
-              <i className='icon iconClose' />
+          <div className="menuItem menuItemHorizontal">
+            <button className="navBtn" onClick={closePreview}>
+              <i className="icon iconClose" />
             </button>
           </div>
         </div>
